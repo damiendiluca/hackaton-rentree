6af314b6190660439bc56a410fb0081b45bf39b1
Damien Di Luca <damiendiluca@MacBook-Air-de-Damien.local>
2025-08-27 12:05:09 +0200

    src/

---
+++ src/controllers/productsController.js
@@ -0,0 +1,84 @@
+import * as Products from "../models/productsModel.js";
+
+export async function listProducts(req, res) {
+  try {
+    const [products] = await Products.getAllProducts();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(err);
+    res.status(500).send("Erreur de serveur");
+  }
+}
+
+export async function addProduct(req, res) {
+  const { name, seller, quantity, price, description, category_id } = req.body;
+  try {
+    await Products.createProducts(
+      name,
+      seller,
+      quantity,
+      price,
+      description,
+      category_id
+    );
+    res.status(201).send("Produit créé avec succès");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+}
+
+export async function putProduct(req, res) {
+  const { id } = req.params;
+  const { name, seller, quantity, price, description, category_id } = req.body;
+  try {
+    await Products.modifyProduct(
+      id,
+      name,
+      seller,
+      quantity,
+      price,
+      description,
+      category_id
+    );
+
+    res.status(201).send("Produit modifié avec succès");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+}
+
+export async function deleteProduct(req, res) {
+  const { id } = req.params;
+  const { name, seller, quantity, price, description, category_id } = req.body;
+  try {
+    await Products.deleteProduct(
+      id,
+      name,
+      seller,
+      quantity,
+      price,
+      description,
+      category_id
+    );
+
+    res.status(201).send("Produit supprimé avec succès");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+}
+
+export async function getProductById(req, res) {
+  const { id } = req.params;
+  try {
+    const [rows] = await Products.getProductById(id);
+    if (!rows.length)
+      return res.status(404).json({ error: "Produit non trouvé" });
+    res.json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+}
---
+++ src/routes/productsRoutes.js
@@ -0,0 +1,19 @@
+import express, { Router } from "express";
+import {
+  listProducts,
+  addProduct,
+  deleteProduct,
+  putProduct,
+  getProductById,
+} from "../controllers/produitsController.js";
+
+const router = express.Router();
+
+router.get("/", listProducts);
+
+router.post("/", addProduct);
+router.delete("/:id", deleteProduct);
+router.put("/:id", putProduct);
+router.get("/:id", getProductById);
+
+export default router;
